9487edd23ca84fce0a90e4351b88956f38ea11ee
Tesfamichael1074 <hope.miky1074@gmail.com>
2020-01-04 11:29:05 +0300

    initial commit with firebase functionality

---
+++ src/components/junk.vue
@@ -0,0 +1,115 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.6.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.6.1/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCn59gNyStOjUB_GL41nvBMcSyz_Tf-9gk",
+    authDomain: "tmeeducation-report.firebaseapp.com",
+    databaseURL: "https://tmeeducation-report.firebaseio.com",
+    projectId: "tmeeducation-report",
+    storageBucket: "tmeeducation-report.appspot.com",
+    messagingSenderId: "757172656806",
+    appId: "1:757172656806:web:ff795863b45b32ff1fb267",
+    measurementId: "G-CK916K5BQ4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
+
+
+
+/////////////////////////////////////////////
+
+<template>
+ <div class="">
+
+<form @submit="login" class="form-signin text-center align-content-center">
+  <img class="mb-4 " src="./assets/tme-education-world-www-kolor.png" alt="" width="280" height="170">
+  <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+  <label for="inputEmail" class="sr-only">Email address</label>
+
+  <input type="email" v-model="email"  id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+  <label for="inputPassword" class="sr-only">Password</label>
+
+  <input type="password" v-model="password" id="inputPassword" class="form-control" placeholder="Password" required>
+  <div class="checkbox mb-3">
+    <label>
+      <input type="checkbox" value="remember-me"> Remember me
+    </label>
+  </div>
+  <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+  <p class="mt-5 mb-3 text-muted">&copy; 2020</p>
+</form>
+
+
+ </div>
+  
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+template {
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 430px;
+  padding: 10px;
+  margin: auto;
+  margin-top: 100px;
+}
+.form-signin .checkbox {
+  font-weight: 400;
+}
+.form-signin .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: 10px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import firebase from "firebase";
+import store from "./store";
+import router from "./routes/index";
 
 Vue.config.productionTip = false
 
+const configOptions = {
+    apiKey: "AIzaSyCn59gNyStOjUB_GL41nvBMcSyz_Tf-9gk",
+    authDomain: "tmeeducation-report.firebaseapp.com",
+    databaseURL: "https://tmeeducation-report.firebaseio.com",
+    projectId: "tmeeducation-report",
+    storageBucket: "tmeeducation-report.appspot.com",
+    messagingSenderId: "757172656806",
+    appId: "1:757172656806:web:ff795863b45b32ff1fb267",
+    measurementId: "G-CK916K5BQ4"
+};
+firebase.initializeApp(configOptions);
+
+firebase.auth().onAuthStateChanged(user => {
+    store.dispatch("fetchUser", user);
+});
+
 new Vue({
-  render: h => h(App),
+    router,
+    store,
+    render: h => h(App),
 }).$mount('#app')
